9ab67ab23c9d8714c3138dcd4696856c81deea91
khoanisdev <doanvankhoan124@gmail.com>
2024-12-29 13:23:17 +0700

    Initial Commit

---
+++ UserNotificationOptions/UserNotificationOptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserNotificationOptionsApp.swift
+//  UserNotificationOptions
+//
+//  Created by Đoàn Văn Khoan on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UserNotificationOptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
